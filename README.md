20b98daf3db83e9aaa9b18bd255a0e2b7ed715da
John DiFelice <johndifelice@protonmail.com>
2021-02-16 20:29:14 -0500

    Fixed issue with utxo[0] address

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Retrieves the sum of all UTXOs for an address using the mattercloud api.
 
 ### getUTXOs
 Retrieves the UTXOs for an address using the mattercloud api.
+
+### sendPayment
+Sends a payment to the specified BSV address from the specified BSV wallet address.
+Built with the txForge library.
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 "use strict";
 
-const axios = require('axios');
-const bsv = require('bsv');
-const {Forge} = require('txforge');
-const https = require('https');
-const { exception } = require('console');
+const axios = require("axios");
+const bsv = require("bsv");
+const {Forge} = require("txforge");
+const https = require("https");
+
 const exchangeApiUrl = "https://api.whatsonchain.com/v1/bsv/main/exchangerate";
+const _privKey = "L1FJLDZWMrBR7JmXKPCfzrUZahBWqLdPaGnDjWQJLJFXAKmvp67V";
+
+const privKey = new bsv.PrivKey().fromString(_privKey);
+const keyPair = new bsv.KeyPair().fromPrivKey(privKey);
 
 module.exports = { 
     
@@ -48,7 +52,7 @@ module.exports = {
         return new Promise(resolve => {
             setTimeout(() => {
                 let utxos;
-                https.get(`https://api.mattercloud.net/api/v3/main/address/${address.toString()}/utxo`, (res) => {
+                https.get(`https://api.mattercloud.net/api/v3/main/address/${fromAddress.toString()}/utxo`, (res) => {
                     let data = '';
                     res.on('data',(chunk) => {
                         data += chunk;
@@ -65,5 +69,97 @@ module.exports = {
                 });    
             },2000);
         });
+    },
+
+
+    sendPayment:async function(_toAddress, _satAmount, _opReturn){
+        var opReturn = _opReturn;
+        var toAddress = _toAddress;
+        var satAmount = _satAmount;
+        
+        return new Promise(resolve => {
+            setTimeout(() => {
+                const fromAddress = "1AMzdZFfkJC7PnxXQndCPKp2q2v8TZSW9E";
+
+                https.get(`https://api.mattercloud.net/api/v3/main/address/1AMzdZFfkJC7PnxXQndCPKp2q2v8TZSW9E/utxo`, (res) => {
+                    let data = '';
+                    res.on('data',(chunk) => {
+                        data += chunk;
+                    });
+
+                    res.on('end', () => {
+                        let utxos = JSON.parse(data); // these are the utxos
+                        let rawTx = buildForge(utxos, toAddress, satAmount, opReturn);
+
+                        postRawTx(rawTx); //UNCOMMENT TO PROCESS PAYMENT
+                        resolve('SUCCESS');
+                    });    
+
+                    res.on('error', error => {
+                        console.error(error)
+                    })
+                })
+            },2000);    
+        })
     }
+    }
+
+    function postRawTx(rawTx){
+        // Send rawTransaction to WhatsOnChain
+        const https = require('https');
+
+        const data = JSON.stringify({
+            txhex: rawTx
+        });
+
+        const options = {
+            hostname: 'api.whatsonchain.com',
+            port: 443,
+            path: '/v1/bsv/main/tx/raw',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
+        }
+
+        const req = https.request(options, res => {
+            console.log(`statusCode: ${res.statusCode}`)
+
+            res.on('data', d => {
+                //process.stdout.write(d)
+                console.log('Post data: ', d);
+            })
+        })
+
+        req.on('error', error => {
+            console.error(error)
+        })
+
+        req.write(data)
+        req.end()
+    }
+
+    function buildForge(_utxo, _toAddress, _satAmount, _opReturn){
+        const opReturnKey = "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut";
+        const privKey = new bsv.PrivKey().fromString(_privKey);
+        const keyPair = new bsv.KeyPair().fromPrivKey(privKey);
+
+        const forge  = new Forge ({
+            inputs: [_utxo],
+            outputs: [
+                {
+                    to: _toAddress,
+                    satoshis: _satAmount
+                },
+                {
+                    data: [opReturnKey,_opReturn,'utf-8']
+                }
+            ],
+            changeTo: _utxo[0].address
+        });
+
+        forge.build().sign({keyPair});
+        
+        return forge.tx.toHex();
 }
